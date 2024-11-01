e03236b1cef3c4ee95db49e8cdfbdf2d2821d2a9
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-11-01 19:17:25 +0100

    Add DocComments

--- sdb.h
+++ sdb.h
@@ -1,3 +1,15 @@
+/**
+ * @file sdb.h
+ * @brief Simple Database Library
+ * @version 0.1.0
+ * 
+ * This is a simple database library that is used to store data in a file.
+ * It is not meant to be a full-featured database, but rather a simple way to store data.
+ * 
+ * @author Johannes (Jotrorox) Müller
+ * @copyright Copyright (c) 2024
+ */
+
 #ifndef SDB_H
 #define SDB_H
 
@@ -7,28 +19,74 @@
 
 #define SDB_VERSION "0.1.0"
 
+/**
+ * @struct SDBEntry
+ * @brief A single entry in the database
+ * 
+ * @var key
+ * @brief The key of the entry
+ * @var value
+ * @brief The value of the entry
+ * @var next
+ * @brief The next entry in the list
+ */
 typedef struct SDBEntry {
     char *key;
     char *value;
     struct SDBEntry *next;
 } SDBEntry;
 
+/**
+ * @struct SDBEntryList
+ * @brief A list of entries
+ * 
+ * @var head
+ * @brief The head of the list
+ * @var tail
+ * @brief The tail of the list
+ */
 typedef struct {
     SDBEntry *head;
     SDBEntry *tail;
 } SDBEntryList;
 
+/**
+ * @struct SDBTable
+ * @brief A table in the database
+ * 
+ * @var name
+ * @brief The name of the table
+ * @var entries
+ * @brief The list of entries in the table
+ */
 typedef struct {
     char *name;
     SDBEntryList *entries;
 } SDBTable;
 
+/**
+ * @struct SDB
+ * @brief The database
+ * 
+ * @var path
+ * @brief The path to the database file
+ * @var tables
+ * @brief The list of tables in the database
+ * @var table_count
+ * @brief The number of tables in the database
+ */
 typedef struct {
     char *path;
     SDBTable *tables;
     int table_count;
 } SDB;
 
+/**
+ * @brief Opens a database file
+ * 
+ * @param path The path to the database file
+ * @return The database
+ */
 SDB* sdb_open(const char* path) {
     SDB* sdb = (SDB*)malloc(sizeof(SDB));
     if (sdb == NULL) {
@@ -99,10 +157,20 @@ SDB* sdb_open(const char* path) {
     return sdb;
 }
 
+/**
+ * @brief Closes the database
+ * 
+ * @param sdb The database
+ */
 void sdb_close(SDB* sdb) {
     free(sdb);
 }
 
+/**
+ * @brief Saves the database
+ * 
+ * @param sdb The database
+ */
 void sdb_save(SDB* sdb) {
     FILE* file = fopen(sdb->path, "wb");
     if (file == NULL) {
@@ -147,6 +215,12 @@ void sdb_save(SDB* sdb) {
     fclose(file);
 }
 
+/**
+ * @brief Creates a table in the database
+ * 
+ * @param sdb The database
+ * @param name The name of the table
+ */
 void sdb_table_create(SDB* sdb, const char* name) {
     sdb->table_count++;
     sdb->tables = (SDBTable*)realloc(sdb->tables, sizeof(SDBTable) * sdb->table_count);
@@ -156,6 +230,12 @@ void sdb_table_create(SDB* sdb, const char* name) {
     sdb->tables[sdb->table_count - 1].entries->tail = NULL;
 }
 
+/**
+ * @brief Destroys a table in the database
+ * 
+ * @param sdb The database
+ * @param name The name of the table
+ */
 void sdb_table_destroy(SDB* sdb, const char* name) {
     for (int i = 0; i < sdb->table_count; i++) {
         if (strcmp(sdb->tables[i].name, name) == 0) {
@@ -172,6 +252,13 @@ void sdb_table_destroy(SDB* sdb, const char* name) {
     }
 }
 
+/**
+ * @brief Finds a table in the database
+ * 
+ * @param sdb The database
+ * @param name The name of the table
+ * @return The table
+ */
 SDBTable* sdb_table_find(SDB* sdb, const char* name) {
     for (int i = 0; i < sdb->table_count; i++) {
         if (strcmp(sdb->tables[i].name, name) == 0) {
@@ -181,6 +268,14 @@ SDBTable* sdb_table_find(SDB* sdb, const char* name) {
     return NULL;
 }
 
+/**
+ * @brief Sets a value in the database
+ * 
+ * @param sdb The database
+ * @param table The name of the table
+ * @param key The key
+ * @param value The value
+ */
 void sdb_table_set(SDB* sdb, const char* table, const char* key, const char* value) {
     SDBTable* t = sdb_table_find(sdb, table);
     if (t == NULL) {
